777c62068059394a27d64da47b069c16997d6299
Jake Ratcliffe <JakeR1342@gmail.com>
2024-02-29 11:25:06 +0000

    Added examples and first example template
    New directory called 'examples' containing simple demonstrations for defined behaviours.
    Each example will have its own project.godot file so it can be opened in the editor and run.

---
+++ examples/README.md
@@ -0,0 +1,1 @@
+A collection of example scenes demonstrating the use of the scripts defined in this repo.
---
+++ examples/inventory/ItemDefinitions.gd
@@ -0,0 +1,3 @@
+class_name ItemData extends Node
+
+var item_definitions: JSON = preload("res://item_definitions.json")
